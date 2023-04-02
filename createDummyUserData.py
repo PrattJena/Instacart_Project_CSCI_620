74d499b497f7c5856f922a24acabf76adcc2ce3d
Anurag Kallurwar <anurag.kallurwar@gmail.com>
2023-04-02 16:01:19 -0400

    Macking Emails Unique

--- createDummyUserData.py
+++ createDummyUserData.py
@@ -1,9 +1,10 @@
 from faker import Faker
 import csv
+import random
 
 
 def writeToCSV():
-    with open('random_us_addresses.csv', mode='w') as file:
+    with open('random_us_addresses2.csv', mode='w') as file:
         fake = Faker()
         writer = csv.writer(file)
         for i in range(4000000):
@@ -15,7 +16,7 @@ def writeToCSV():
             zipcode = fake.zipcode()
             email = fake.domain_name()
             nameArr = name.lower().split()
-            userEmail = nameArr[0]+nameArr[1]+street_address_Arr[0]+"@"+email
+            userEmail = nameArr[0]+nameArr[1]+street_address_Arr[0]+city+str(random.randint(0, 10000))+"@"+email
             writer.writerow([i, name, street_address, city, state, zipcode, userEmail])
 
 writeToCSV()
